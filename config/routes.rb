af3655f98523166c6405d0960f32a85a128357e1
sergio rodas <rodascruzsergio@gmail.com>
2022-05-20 17:40:50 -0400

    test the all models and controllers created

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :places do
+    resources :review_places, only: :create
+    resources :publications, only: [:new, :create, :edit, :update]
+  end
+
+  resources :publications, only: [:index, :show, :destroy] do
+    resources :review_publications, only: :create
+  end
+
+  resources :review_publications, only: :destroy
+
+  resources :review_places, only: :destroy
 end
---
+++ test/controllers/review_places_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ReviewPlacesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
