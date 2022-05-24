1c6a25b999ecd5a70092eb0fb94455389ecfe631
sergio rodas <rodascruzsergio@gmail.com>
2022-05-24 11:25:34 -0400

    config aplication js and leoyouts aplications

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,12 @@ ActiveStorage.start()
 
 import "controllers"
 import "bootstrap"
+
+// Stimulus import
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+// Stimulus
+const application = Application.start()
+const context = require.context("controllers", true, /_controller\.js$/)
+application.load(definitionsFromContext(context))
