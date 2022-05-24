450e29e9eb588a8d38e931022d3f4f2f096e095c
sergio rodas <rodascruzsergio@gmail.com>
2022-05-24 10:12:29 -0400

    added validations the cloudinary at models

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,5 +2,7 @@ class Place < ApplicationRecord
   has_many :publications
   has_many :review_places
 
+  has_one_attached :photo
+
   validates :name, :description, :rating, presence: true
 end
--- app/models/publication.rb
+++ app/models/publication.rb
@@ -3,6 +3,7 @@ class Publication < ApplicationRecord
   belongs_to :place
 
   has_many :review_publications
+  has_many_attached :photos
 
   validates :title, :description, presence: true
 end
