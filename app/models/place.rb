a2142d7219996f75b4c0a771e69bffa6b447cfb0
sergio rodas <rodascruzsergio@gmail.com>
2022-05-25 01:34:11 -0400

    added validations and dates in the models

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,7 +2,9 @@ class Place < ApplicationRecord
   has_many :publications, dependent: :destroy
   has_many :review_places, dependent: :destroy
 
-  has_one_attached :photo, dependent: :destroy
+  has_many_attached :photos, dependent: :destroy
 
   validates :name, :description, :rating, presence: true
+  validates :city, :recomendation, :other_recomendation, presence: true
+  validates :how_to_get, :what_to_bring, presence: true
 end
--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,9 +1,13 @@
 class Publication < ApplicationRecord
   belongs_to :user
   belongs_to :place
+  belongs_to :category
 
   has_many :review_publications, dependent: :destroy
   has_many_attached :photos, dependent: :destroy
 
   validates :title, :description, presence: true
+
+  enum status: [ :pending, :active, :deleted ], _default: :pending
+
 end
